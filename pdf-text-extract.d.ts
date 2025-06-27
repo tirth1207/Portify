b31eb659c1298374192a620ce6b3814fce52fa46
Tirth Rathod <rathodtirth1207@gmail.com>
2025-06-27 15:49:13 +0530

    fixed

--- pdf-text-extract.d.ts
+++ pdf-text-extract.d.ts
@@ -1,8 +1,21 @@
-declare module 'pdf-text-extract' {
-  function extract(
-    file: Buffer | string,
-    options?: any,
-    callback?: (err: Error | null, pages: string[]) => void
-  ): void;
-  export = extract;
-} 
+declare module 'pdf-parse' {
+  interface PDFParseOptions {
+    max?: number;
+  }
+
+  interface PDFParseData {
+    numpages: number;
+    numrender: number;
+    info: Record<string, any>;
+    metadata: any;
+    text: string;
+    version: string;
+  }
+
+  function pdfParse(
+    buffer: Buffer,
+    options?: PDFParseOptions
+  ): Promise<PDFParseData>;
+
+  export default pdfParse;
+}
