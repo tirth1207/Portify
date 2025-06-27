4eb74b16ef01ff5eeb3a344db4a5a8b696e09308
Tirth Rathod <rathodtirth1207@gmail.com>
2025-06-27 15:50:52 +0530

    fixed

--- components/magicui/blur-fade-text.tsx
+++ components/magicui/blur-fade-text.tsx
@@ -45,7 +45,7 @@ const BlurFadeText = ({
               exit="hidden"
               variants={combinedVariants}
               transition={{
-                yoyo: Infinity,
+                // yoyo: Infinity,
                 delay: delay + i * characterDelay,
                 ease: "easeOut",
               }}
@@ -69,7 +69,7 @@ const BlurFadeText = ({
           exit="hidden"
           variants={combinedVariants}
           transition={{
-            yoyo: Infinity,
+            // yoyo: Infinity,
             delay,
             ease: "easeOut",
           }}
